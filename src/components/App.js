5f4a7921bfdfd8faaaedd058c87c5f2bb998de4c
Yulia <sivonenkoyulia@gmail.com>
2021-01-20 02:58:44 +0200

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,40 @@
-
 import { Component } from 'react'
 import Container from './Container'
 import Section from './Section'
 import FeedbackOptions from './FeedbackOptions'
 import Statistics from './Statistics'
+import { fdbkOptions } from '../data/constans'
 
-import{fdbkOptions} from '../data/constans'
 class App extends Component {
   state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+    good: 0,
+    neutral: 0,
+    bad: 0
   }
+
   handleaveFeedback = ({ target }) => {
     const { feedback } = target.dataset
-    this.setState(prevState => ({[feedback]: prevState[feedback] + 1}))
+    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }))
   }
+
   countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state 
+    const { good, neutral, bad } = this.state
     return good + neutral + bad
   }
+
   countPositiveFeedbackPercentage = () => {
     const { good } = this.state;
     const total = this.countTotalFeedback()
-    return total ?Math.round((good / (total)) * 100) : 0
+    return total ? Math.round((good / (total)) * 100) : 0
   }
 
-  
+
   render() {
     const { good, neutral, bad } = this.state
-    
+
     return <Container>
       <Section title='Please leave feedback'>
-        <FeedbackOptions options={fdbkOptions} onleaveFeedback={this.handleaveFeedback}/>
+        <FeedbackOptions options={fdbkOptions} onleaveFeedback={this.handleaveFeedback} />
       </Section>
       <Section title='Statistics'>
         <Statistics
